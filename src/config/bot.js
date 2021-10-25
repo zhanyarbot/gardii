21ba9e5740bbb0ae5134c6306dcce4e6079a1a48
zhanyarbot <92117702+zhanyarbot@users.noreply.github.com>
2021-10-25 23:26:22 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    token: 'OTAxMTIwMzAyMzA4Nzg2MjI2.YXLPrg.yg-Re2tSbgqSxp4C-pPPrX12Svo',
+    token: 'OTAxMTIwMzAyMzA4Nzg2MjI2.YXLPrg.2wbeENgrNr8I6IzQndY69dCo054',
     prefix: ','
 }
